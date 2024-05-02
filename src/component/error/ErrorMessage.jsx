00a79054ffe308cfc8f3b3a39d76109bd414eed6
keraberas <46618649+keraberase@users.noreply.github.com>
2024-05-02 14:54:58 +0300

    redact error

---
+++ src/component/error/ErrorMessage.jsx
@@ -0,0 +1,11 @@
+import toast, { Toaster } from "react-hot-toast";
+
+const notify = () =>
+  toast("Whoops, something went wrong! Please try reloading this page!");
+
+const ErrorMessage = () => {
+  notify();
+  return <Toaster />;
+};
+
+export default ErrorMessage;
---
+++ src/redux/filtersSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const InitialFilters = {
+  filters: {
+    name: "",
+  },
+};
+
+const filtersSlice = createSlice({
+  name: "filters/name",
+  initialState: InitialFilters,
+  reducers: {
+    changeFilter: (state, action) => {
+      state.filters.name = action.payload;
+    },
+  },
+});
+
+export const { changeFilter } = filtersSlice.actions;
+
+export const filtersReducer = filtersSlice.reducer;
